e68555968c7871cfa2694737179ebc7b9c7d62a9
Marcin Gasior <mgasior@groupon.com>
2014-11-05 19:20:19 +0000

    node style

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 var app = require('express')()
-var Q = require('q')
-var request = Q.denodeify(require('request'))
+var async = require('async');
+var request = require('request');
+var _ = require('underscore');
 
 app.get('/', function(req, res) {
   // get a random number
-  request('http://randnum.herokuapp.com').then(function(response) {
-    return parseInt(response[0].body);
-  }).then(function(numWords) {
-    // request random words
-    var requests = Array.apply(null, new Array(numWords)).map(function() {
-      return request('http://random-word.herokuapp.com')
-    })
+  function getPage(url){
+      return function(callback){
+          request(url, function(err, resp, body){
+              callback(null, body);
+          })
+      }
 
-    return Q.all(requests)
-  }).then(function(responses) {
-    // return the random words in the response
-    var words = responses.map(function(response) {
-      return response[0].body
-    })
+  }
 
-    res.send(words.join(" "))
-  })
+  async.auto({
+      "step1": getPage('http://randnum.herokuapp.com'),
+      "step2": ['step1', getPage('http://random-word.herokuapp.com')]
+  }, function(err, results){
+      if (!err){
+          res.send(_.values(results).join(','));
+      }else{
+          console.log("ERROR: "+err);
+      }
+
+  });
 })
 
 var server = app.listen((process.env.PORT || 5000), function() {
